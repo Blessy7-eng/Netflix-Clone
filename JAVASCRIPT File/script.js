dadd8fb86a8104fc357bb2034a20d78c932acc1a
Blessy Waydande <waydandeblessy22@gmail.com>
2025-08-22 21:08:54 +0530

    Create script.js

---
+++ JAVASCRIPT File/script.js
@@ -0,0 +1,12 @@
+// const scrollButton = document.querySelector('.scroll-btn');
+// const scrollableContent = document.querySelector('.movie');
+// const leftButton = document.querySelector('.left-btn');
+// const rightButton = document.querySelector(".right-btn");
+
+// leftButton.addEventListener('click', () => {
+//     scrollableContent.scrollLeft -= 100;
+// });
+
+// rightButton.addEventListener('click', () => {
+//     scrollableContent.scrollLeft += 100;
+// });
